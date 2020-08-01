79fed14a2f36276ad26516fc5074e38e3d42bb4f
Kenny Howard <ken.chad.howard@gmail.com>
2020-07-31 18:19:53 -0600

    rearranging some things

--- src/components/Navigation/Header.js
+++ src/components/Navigation/Header.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-import SignedInLinks from './SignedInLinks';
-import SignedOutLinks from './SignedOutLinks';
-
 import './Header.scss';
 
 const Header = () => {
@@ -13,7 +10,38 @@ const Header = () => {
 				Running Backs of Scotland
 			</NavLink>
 			<div className='links-container'>
-				<SignedInLinks />
+				<ul className='nav-link-container'>
+					<li>
+						<NavLink className='nav-link' to='/members'>
+							Members
+						</NavLink>
+					</li>
+					<li>
+						<NavLink className='nav-link' to='/constitution'>
+							Constitution
+						</NavLink>
+					</li>
+					<li>
+						<NavLink className='nav-link' to='/rules'>
+							Rules
+						</NavLink>
+					</li>
+					<li>
+						<NavLink className='nav-link' to='/bets'>
+							Bets
+						</NavLink>
+					</li>
+					<li>
+						<NavLink className='nav-link' to='/logout'>
+							Logout
+						</NavLink>
+					</li>
+					<li>
+						<NavLink className='nav-link' to='/'>
+							KH
+						</NavLink>
+					</li>
+				</ul>
 			</div>
 		</nav>
 	);
