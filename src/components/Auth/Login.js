7423921e82aecba786ac434198de000128c7899c
Kenny Howard <ken.chad.howard@gmail.com>
2020-08-03 19:33:11 -0600

    styling login page, need to do some extra work to it later

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
 
+import './auth.scss';
+
 const Login = () => {
 	const [login, setLogin] = useState(true);
 
 	return (
 		<div className='login'>
 			<h2>{login ? 'Login' : 'Create An Account'}</h2>
-			<form>
+			<form className='login-form'>
 				{!login && (
 					<input type='text' placeholder='please enter your name' autoComplete='off' />
 				)}
 				<input type='email' placeholder='Please enter your email' autoComplete='off' />
 				<input type='password' placeholder='Password' autoComplete='off' />
-				<div>
+				<div className='login-buttons'>
 					<button type='submit'>Submit</button>
 					<button type='button' onClick={() => setLogin(prevLogin => !prevLogin)}>
 						{login ? 'Need to create and account?' : 'Already have an account?'}
