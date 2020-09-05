0fc1cc762064ddba710081b27817a61eb6d9928e
Kenny Howard <ken.chad.howard@gmail.com>
2020-09-04 18:04:53 -0600

    forget password works - need to style it

--- src/components/Auth/ForgotPassword.js
+++ src/components/Auth/ForgotPassword.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { FirebaseContext } from '../../firebase';
+
+import './auth.scss';
 
 const ForgotPassword = () => {
+	const { firebase } = useContext(FirebaseContext);
+	const [resetPasswordEmail, setResetPasswordEmail] = useState('');
+	const [isPasswordReset, setIsPasswordReset] = useState(false);
+	const [passwordResetError, setPasswordResetError] = useState(null);
+
+	async function handleResetPassword() {
+		try {
+			await firebase.resetPassword(resetPasswordEmail);
+			setIsPasswordReset(true);
+			setPasswordResetError(null);
+		} catch (err) {
+			console.error('Error sending email', err);
+			setPasswordResetError(err.message);
+			setIsPasswordReset(false);
+		}
+	}
+
 	return (
-		<div className='forgot-password'>
-			<h2>Forgot Password</h2>
+		//TODO Need to style this much better
+		<div className='forgot-password-container'>
+			<input
+				type='email'
+				className='input'
+				placeholder='Provide your email'
+				onChange={event => setResetPasswordEmail(event.target.value)}
+			/>
+			<div>
+				<button className='button' onClick={handleResetPassword}>
+					Reset Password
+				</button>
+			</div>
+			{isPasswordReset && <p>Check email to reset password</p> }
+			{passwordResetError && <p className='error-text'>{passwordResetError}</p>}
 		</div>
 	);
 };
