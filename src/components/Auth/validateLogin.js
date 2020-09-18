af7f2ac9050dcd7326f10ace1338ccadf1f21d29
Kenny Howard <ken.chad.howard@gmail.com>
2020-09-18 15:17:55 -0600

    going to temporarily comment this out

--- src/components/Auth/validateLogin.js
+++ src/components/Auth/validateLogin.js
@@ -4,14 +4,14 @@ export default function validateLogin(values) {
 
 	// Email Errors
 	if (!values.email) {
-		errors.email = 'Email required';
+		// errors.email = 'Email required';
 	} else if (!validEmail) {
 		errors.email = 'Invalid email address';
 	}
 
 	// Password Errors
 	if (!values.password) {
-		errors.password = 'Password required';
+		// errors.password = 'Password required';
 	} else if (values.password.length < 6) {
 		errors.password = 'Password must be at least 6 characters long';
 	}
