ff7f0cbb157b3e90340ec142c63212647a9463ca
Kenny Howard <ken.chad.howard@gmail.com>
2020-09-18 15:13:58 -0600

    a better fix

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -30,7 +30,7 @@ const Login = props => {
 				? await firebase.login(email, password)
 				: await firebase.register(name, email, password)
 			props.history.push('/'); 
-			props.history.go(0);
+			{!login && props.history.go(0);}
 		} catch (err) {
 			console.error('Authentication Error', err);
 			setFirebaseError(err.message)
