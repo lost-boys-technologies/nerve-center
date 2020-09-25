e8ebf5e0a1dd5b69b5de634b5abc6e97f0e634a7
Kenny Howard <ken.chad.howard@gmail.com>
2020-09-25 02:39:36 -0600

    bringing in my favorite 404

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,7 @@ import Homepage from './StaticPages/Homepage';
 import Constitution from './StaticPages/Constitution';
 import Rules from './StaticPages/Rules';
 import Account from './StaticPages/Account';
+import NotFound from './StaticPages/404';
 
 import './App.scss';
 
@@ -43,6 +44,7 @@ const App = () => {
 						)}
 						<Route path='/login' component={Login} />
 						<Route path='/forgot' component={ForgotPassword} />
+						<Route component={NotFound}/>
 					</Switch>
 				</div>
 			</FirebaseContext.Provider>
