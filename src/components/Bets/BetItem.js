8ef4765fb3ed6590e23dc3651cf45e6f05cc3633
Kenny Howard <ken.chad.howard@gmail.com>
2021-02-10 00:16:13 -0700

    current approach

--- src/components/Bets/BetItem.js
+++ src/components/Bets/BetItem.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Collapse from 'react-bootstrap/Collapse';
+import Button from 'react-bootstrap/Button';
 
 // TODO Will need to setup additional validation to only allow approval voting BY the person being challenged
 
 const BetItem = ({ bet, index, showCount }) => {
-    const { challenger, betDetails, dateCompletion, pending } = bet;
-
-    //? Need to rethink the below split, not sure if I like this process
+    const [open, setOpen] = useState(false);
+    const { challenger, betDetails, dateCompletion } = bet;
 
     return (
         <div className='bet-item-container'>
@@ -17,8 +18,22 @@ const BetItem = ({ bet, index, showCount }) => {
             </div>
             <div className='bet-voting'>
                 {showCount && <span>{index}</span>}
-                <div className='bet-approval'><i class='far fa-thumbs-up'></i></div>
-                <div className='bet-rejection'><i class='far fa-thumbs-down'></i></div>
+                <div className='bet-approval'><i className='far fa-thumbs-up'></i></div>
+                <div className='bet-rejection'><i className='far fa-thumbs-down'></i></div>
+            </div>
+            <div>
+                <Button
+                    onClick={() => setOpen(!open)}
+                    aria-controls="example-collapse-text"
+                    aria-expanded={open}
+                >
+                    click
+                </Button>
+                <Collapse in={!open}>
+                    <div id="example-collapse-text">
+                        {betDetails}
+                    </div>
+                </Collapse>
             </div>
         </div>
     )
