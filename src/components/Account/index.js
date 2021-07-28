22d4d52cd938b7d2d37d6a5f0b28a11c6a450e40
Kenny Howard <ken.chad.howard@gmail.com>
2021-07-28 01:24:35 -0600

    rough go and I will need to update my snapshot

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import FirebaseContext from '../../firebase/context';
 import Grid from '@material-ui/core/Grid';
 
@@ -6,11 +6,28 @@ import './account.scss';
 
 const Account = () => {
 	const { firebase, user } = useContext(FirebaseContext);
+	const [currentUser, setCurrentUser] = useState([])
 
-	console.log('displayName', user.displayName);
-	console.log('email', user.email);
-	console.log('member since', user.metadata.creationTime);
-	console.log('user', user);
+	useEffect(() => {
+        getUsers();
+    }, [])
+
+	const getUsers = () => {
+        firebase.db.collection('users').onSnapshot(handleSnapshot);
+    }
+
+	// TODO Cleanup with CreateBet.js `handleSnapshot` very similar
+	const handleSnapshot = (snapshot) => {
+        const users = snapshot.docs.map(doc => {
+            return { id: doc.id, ...doc.data() }
+        });
+        const currentUser = users.filter(filteredUser => filteredUser.uid === user.uid);
+        setCurrentUser(currentUser[0]);
+    }
+
+	// TODO Create admin portal to update user details
+	const { admin } = currentUser
+	console.log('admin', admin);
 
 	return (
 		<div className='my-account-container'>
