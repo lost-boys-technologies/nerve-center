43baf14440136985320317622e7f81f6508e8958
Kenny Howard <ken.chad.howard@gmail.com>
2022-03-04 00:01:30 -0700

    data connection - initial layout

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import FirebaseContext from '../../firebase/context';
 
 import './admin.scss';
 
 const Constitution = () => {
+	const { firebase } = useContext(FirebaseContext);
+	const [allUsers, setAllUsers] = useState([]);
+
+	useEffect(() => {
+        getUsers();
+    }, [])
+
+	const getUsers = () => {
+        firebase.db.collection('users').onSnapshot(handleSnapshot);
+    }
+
+	const handleSnapshot = (snapshot) => {
+        const users = snapshot?.docs?.map(doc => {
+            return { id: doc?.id, ...doc?.data() }
+        });
+		console.log('users', users);
+		setAllUsers(users);
+    }
+
 	return (
-		<div className='admin'>
-			<h2>Admin Portal</h2>
+		<div className='admin-container'>
+			<div className='league-members'>
+				{allUsers.map(user => {
+					return (
+						<div className='name'>
+							{user.name}
+						</div>
+					)
+				})}
+			</div>
 		</div>
 	);
 };
